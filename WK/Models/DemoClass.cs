f97f73111833cd686c2e984dfddcd2308a775cf5
Monica <wkon0005@student.monash.edu>
2019-09-14 10:16:59 +1000

    Add project files.

---
+++ WK/Models/DemoClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WK.Models
+{
+    public class DemoClass
+    {
+    }
+
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+}
---
+++ WK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WK.Startup))]
+namespace WK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
